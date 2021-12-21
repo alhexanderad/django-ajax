8118c944467594f03fd2f4d9a4e7e5d3b9d6ac1c
alhexanderad <alhexanderad@gmail.com>
2021-12-21 15:07:07 -0500

    Se realizo la configuracion de media static, la creacion de models profiles y posts

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,19 @@
 from django.db import models
+from django.db.models.fields.related import ForeignKey, ManyToManyField
+from django.contrib.auth.models import User
+from profiles.models import Profile
+class Post(models.Model):
+  title = models.CharField(max_length=200)
+  body = models.TextField()
+  liked = models.ManyToManyField(User, blank=True)
+  author = models.ForeignKey(Profile, on_delete=models.CASCADE)
+  updated = models.DateTimeField(auto_now=True)
+  created = models.DateTimeField(auto_now_add=True)
+  class Meta:
+    """Meta definition for Post."""
 
-# Create your models here.
+    verbose_name = 'Post'
+    verbose_name_plural = 'Posts'
+
+  def __str__(self):
+    return str(self.title)
