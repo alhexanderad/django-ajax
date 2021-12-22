824dc8b560b730f7e9a091063eb863d8af9dbe0a
alhexanderad <alhexanderad@gmail.com>
2021-12-21 23:55:38 -0500

    se realizo static main.js de posts para poder manipular ajax

--- core/settings.py
+++ core/settings.py
@@ -45,7 +45,7 @@ ROOT_URLCONF = 'core.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -111,6 +111,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS =[
   BASE_DIR / 'static',
+  BASE_DIR / 'posts' /'static',
+  BASE_DIR / 'profiles' /'static',
 ]
 
 
