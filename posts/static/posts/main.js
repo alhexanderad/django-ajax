7f0d3c389b102b7741413437d8ac1e7a286fd559
alhexanderad <alhexanderad@gmail.com>
2022-01-05 18:47:41 -0500

    Se realizo la parte de get con json y spinner

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,8 +1,8 @@
-console.log('Hola Mundo estamos de vuelta 2022');
+console.log('Loremamdkfkasdfksadkfas,d');
 
 const helloWorldBox = document.getElementById('hello-world');
-
-
+const postsBox = document.getElementById('posts-box');
+const spinnerBox = document.getElementById('spinner-border');
 
 $.ajax({
   type: 'GET',
@@ -13,5 +13,27 @@ $.ajax({
   },
   error: function(error){
     console.log('error', error);
+  }
+})
+
+$.ajax({
+  type:'GET',
+  url:'data/',
+  success:function(response){
+    console.log('Informacion de Get data ajax');
+    console.log(response)
+    const data = response.data
+    setTimeout(()=>{
+      spinnerBox.classList.add('not-visible');
+      console.log(data);
+      data.forEach(el => {
+        postsBox.innerHTML +=`
+          ${el.title} - <b>${el.body}</b><br>
+          `
+      });
+    }, 1000)
+  },
+  error: function(error){
+    console.log(error);
   }
 })
