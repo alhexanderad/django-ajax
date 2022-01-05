5bc867b9e19ec8cb8f5bb6470032329aebd7c097
alhexanderad <alhexanderad@gmail.com>
2022-01-05 12:49:49 -0500

    Se realizo ajax get hello-word para pode mostrar en console.log

--- core/urls.py
+++ core/urls.py
@@ -5,7 +5,7 @@ from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('post/', include('posts.urls', namespace='posts')),
+    path('posts/', include('posts.urls', namespace='posts')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -1,1 +1,17 @@
-console.log('Hola Mundo');
+console.log('Hola Mundo estamos de vuelta 2022');
+
+const helloWorldBox = document.getElementById('hello-world');
+
+
+
+$.ajax({
+  type: 'GET',
+  url:' hello-world/',
+  success: function(response){
+    console.log('success',response.text)
+    helloWorldBox.textContent = response.text
+  },
+  error: function(error){
+    console.log('error', error);
+  }
+})
