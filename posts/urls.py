b609411a3b2c0288c1a3af23b1ae4b4e76b8bd4e
alhexanderad <alhexanderad@gmail.com>
2022-01-13 23:40:28 -0500

    se realizo la visualizacio de informacion por ajax

--- posts/urls.py
+++ posts/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
-from .views import post_list_and_create, hello_world_view, load_post_data_view,like_unlike_post, post_detail
+from .views import (post_list_and_create, 
+                    hello_world_view,load_post_data_view,like_unlike_post, post_detail, post_detail_data_view)
 
 app_name = 'posts'
 
@@ -7,6 +8,7 @@ urlpatterns = [
     path('', post_list_and_create, name = 'main-board'),
     path('like_unlike/', like_unlike_post, name='like-unlike'),
     path('<pk>/', post_detail, name='post-detail'),
+    path('<pk>/data/', post_detail_data_view, name='post-detail-view'),
     path('data/<int:num_posts>/', load_post_data_view, name = 'posts-data'),
     path('hello-world/', hello_world_view, name = 'hello-world'),
     
--- posts/views.py
+++ posts/views.py
@@ -1,3 +1,4 @@
+from json.encoder import JSONEncoder
 from django.shortcuts import render
 from .models import Post
 from django.core import serializers
@@ -66,6 +67,17 @@ def load_post_data_view(request, num_posts):
       }
       data.append(item)
     return JsonResponse({'data':data[lower:upper], 'size':size})
+
+def post_detail_data_view(request, pk):
+  obj= Post.objects.get(pk=pk)
+  data = {
+    'id': obj.id,
+    'title': obj.title,
+    'body': obj.body,
+    'author': obj.author.user.username,
+    'logged_in': request.user.username,
+  }
+  return JsonResponse({'data':data})
 
 def like_unlike_post(request):
   if request.is_ajax():
