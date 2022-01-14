e5806cb31c9bcae43a960a92cb76409935d419d7
alhexanderad <alhexanderad@gmail.com>
2022-01-13 20:02:17 -0500

    Se realizo la parte de windows.location.herf para poder identicar el url y ingresar el id

--- posts/static/posts/main.js
+++ posts/static/posts/main.js
@@ -11,6 +11,10 @@ const title = document.getElementById('id_title')
 const body = document.getElementById('id_body')
 const csrf = document.getElementsByName('csrfmiddlewaretoken')
 
+const url = window.location.href
+// console.log('location abajo');
+// console.log(window.location);
+
 const alertBox = document.getElementById('alert-box')
 
 console.log('Mostrando csrf');
@@ -95,7 +99,7 @@ const getData = () =>{
               <div class ="card-footer">
                 <div class="row">
                   <div class="col-2">
-                    <a href="#" class="btn btn-primary">Details</a>
+                    <a href="${url}${el.id}" class="btn btn-primary">Details</a>
                   </div>
                   <div class="col-2">
                     <form class="like-unlike-forms" data-form-id="${el.id}">
--- posts/urls.py
+++ posts/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
-from .views import post_list_and_create, hello_world_view, load_post_data_view,like_unlike_post
+from .views import post_list_and_create, hello_world_view, load_post_data_view,like_unlike_post, post_detail
 
 app_name = 'posts'
 
 urlpatterns = [
     path('', post_list_and_create, name = 'main-board'),
     path('like_unlike/', like_unlike_post, name='like-unlike'),
+    path('<pk>/', post_detail, name='post-detail'),
     path('data/<int:num_posts>/', load_post_data_view, name = 'posts-data'),
     path('hello-world/', hello_world_view, name = 'hello-world'),
     
