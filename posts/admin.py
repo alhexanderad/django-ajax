a80a11d21397150bd256fc0e4547768c8cb3539a
alhexanderad <alhexanderad@gmail.com>
2022-01-14 23:47:35 -0500

    Se creo la tabla photo para post

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,7 @@
+import site
 from django.contrib import admin
 from django.contrib.admin.decorators import register
-from .models import Post
+from .models import Post, Photo
 
 admin.site.register(Post)
-
+admin.site.register(Photo)
--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,5 @@
+from email.mime import image
+from pyexpat import model
 from django.db import models
 from django.db.models.fields.related import ForeignKey, ManyToManyField
 from django.contrib.auth.models import User
@@ -15,6 +17,8 @@ class Post(models.Model):
     verbose_name = 'Post'
     verbose_name_plural = 'Posts'
 
+  def get_photos(self):
+    return self.photo_set.all()
 
   def __str__(self):
     return str(self.title)
@@ -23,3 +27,10 @@ class Post(models.Model):
   def like_count(self):
     return self.liked.all().count()
   
+class Photo(models.Model):
+  post = models.ForeignKey(Post, on_delete=models.CASCADE)
+  image = models.ImageField(upload_to="photos")
+  created = models.DateTimeField(auto_now_add=True)
+  
+  def __str__(self):
+      return f"{self.post.title}-{self.pk}"
