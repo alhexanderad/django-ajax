13d24740e1b48efda87fa881628d18b149a199dd
alhexanderad <alhexanderad@gmail.com>
2022-01-18 22:44:34 -0500

    Se realizo para parte de forms, urls, template de profiles

--- core/urls.py
+++ core/urls.py
@@ -6,6 +6,7 @@ from django.conf import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('posts/', include('posts.urls', namespace='posts')),
+    path('profiles/', include('profiles.urls', namespace='profile')),
 ]
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
---
+++ profiles/static/js/main.js
@@ -0,0 +1,1 @@
+console.log("Hola esto es mi profiles para mi");
